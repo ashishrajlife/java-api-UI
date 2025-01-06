88b18f14614c8082783995f5d6048fa27cc5d321
Ashish raj <142250331+ashishrajlife@users.noreply.github.com>
2025-01-07 00:31:04 +0530

    Add files via upload
    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Initialize AOS animations (if you're using AOS library for scroll animations)
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+      duration: 1200,
+      once: true, // Animations will only happen once
+    });
+  });
+  
